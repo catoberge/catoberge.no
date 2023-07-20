8308be7597c02a89952984b957f8157fe1a381ed
Cato Berge <80479739+catoberge@users.noreply.github.com>
2023-07-20 20:41:37 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// Nav bar
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+// Image gallery
+
+const gallery = document.getElementById("gallery");
+const popup = document.getElementById("popup");
+const selectedImage = document.getElementById("selectedImage");
+const imageIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const selectedIndex = null;
+
+imageIndexes.forEach((i) => {
+    const image = document.createElement("img");
+    image.src = `/Image gallery/img/${i}.jpg`;
+    image.alt = `Bilde ${i} av mitt galleri`;
+    image.classList.add("galleryImg");
+
+    image.addEventListener("click", () => {
+        // popup
+        popup.style.transform = `translateY(0)`;
+        selectedImage.src = `/Image gallery/img/${i}.jpg`;
+        selectedImage.alt = `Bilde ${i} av mitt galleri`;
+
+    });
+
+    gallery.appendChild(image);
+});
+
+popup.addEventListener("click", () => {
+    popup.style.transform = `translateY(-100%)`;
+    popup.src = "";
+    popup.alt = "";
+});
