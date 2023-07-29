38eea722c4be9227480f88df2dc53d751478e908
Cato Berge <80479739+catoberge@users.noreply.github.com>
2023-07-29 16:39:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const selectedIndex = null;
 
 imageIndexes.forEach((i) => {
     const image = document.createElement("img");
-    image.src = `/img/${i}.jpg`;
+    image.src = `img/${i}.jpg`;
     image.alt = `Bilde ${i} av mitt galleri`;
     image.classList.add("galleryImg");
 
